41de715950221d8abe2fc0a019145721570381a7
Muhammad Jidan <muhammadjidan703@gmail.com>
2026-02-22 22:56:02 +0700

    feat 1. update logic cd in deploy.yml 2. add route web.php for testing cd

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/jidan', function () {
+    return response()->json([
+        'first_name' => 'Muhammad',
+        'last_name' => 'Jidan',
+        'age' => 21,
+    ]);
+});
